5bf412180d185915559d1bbcad8449006100c43b
Teresa <teresa.lenz@desy.de>
2018-10-22 15:11:48 +0200

    implement inheritance

--- DNN/GoF/produceGoFInput.h
+++ DNN/GoF/produceGoFInput.h
@@ -8,77 +8,67 @@
 #include "Unfold.C"
 #include "CMS_lumi.C"
 
-class analyzedInstance {
+class analyzer_instance_2D {
 
 public :
   TString name;
-  TString cutString = "";
-  TString cutStringSS = "";
-  TString cutStringSSrelaxed = "";
+
+  TString cutString    = "";
   TString weightString = "";
+  TString cutStringSS    = "";
   TString weightStringSS = "";
+  TString cutStringSSrelaxed    = "";
   TString weightStringSSrelaxed = "";
+
   TString topweight     = "";
   TString zptmassweight = "";
+  TString qcdweight     = "";
   TString ggscaleweight = "";
-  TString qcdweight = "";
+
   TH2D *hist;
   TH2D *histSS;
   TH2D *histSSrelaxed;
-  TString variable;
-  sysUncertainty(TString sysName=""){
-    name = sysName;
-    hist          = new TH2D(name+"_os","",100,0,200,100,0,200);
-    histSS        = new TH2D(name+"_ss","",100,0,200,100,0,200);
-    histSSrelaxed = new TH2D(name+"_ss_relaxed","",100,0,200,100,0,200);
+  
+  TString variable = "m_vis : pt_2";
+
+  analyzer_instance_2D(TString name = ""){
+    name = name;
+    /* hist          = new TH2D(name+"_os","",100,0,200,100,0,200); */
+    /* histSS        = new TH2D(name+"_ss","",100,0,200,100,0,200); */
+    /* histSSrelaxed = new TH2D(name+"_ss_relaxed","",100,0,200,100,0,200); */
   }
 };
 
 
-class sysUncertainty {
+class sysUncertainty : public analyzer_instance_2D {
 
 public :
-  TString name;
-  TString cutString = "";
-  TString weightString = "";
-  TString topweight = "topptweightRun2*";
-  TString zptmassweight = "zptmassweight*";
-  TString ggscaleweight = "";
-  TString treeVar;
-  TH2D *hist;
-  TH2D *histSSrelaxed;
-  TString variable;
-  sysUncertainty(TString sysName=""){
+  
+  TString sysName;
+  
+  sysUncertainty(TString sysName = ""){
     name = sysName;
-    hist          = new TH2D("","",100,0,200,100,0,200);
-    histSSrelaxed = new TH2D("","",100,0,200,100,0,200);
+    /* hist          = new TH2D("","",100,0,200,100,0,200); */
+    /* histSSrelaxed = new TH2D("","",100,0,200,100,0,200); */
   }
 };
 
-class sample {
+class sample : public analyzer_instance_2D {
 
 public :  
-  TString name;
+
   TString filename;
-  TString cutString;
-  TString cutStringSS;
-  TString cutStringSSrelaxed;
-  TString weightString;
-  TString weightStringSS;
-  TString weightStringSSrelaxed;
-  TH2D *hist;
-  TH2D *histSS;
-  TH2D *histSSrelaxed;
-  TString variable;
   vector<sysUncertainty> uncertainties;
   double norm = 1;
  
-  sample(TString sampleName="", TString filename=""){
-    name = sampleName;
-    filename = filename;
-    hist          = new TH2D(name+"_os","",100,0,200,100,0,200);
-    histSSrelaxed = new TH2D(name+"_ss_relaxed","",100,0,200,100,0,200);
-  }
+ sample(TString sampleName = "", TString filepath = "")
+    {
+      name     = sampleName;
+      filename = filepath;
+      cout<<filename<<endl;
+      /* hist          = new TH2D(name+"_os","",100,0,200,100,0,200); */
+      /* histSSrelaxed = new TH2D(name+"_ss_relaxed","",100,0,200,100,0,200); */
+    }
 };
 
 class category {
