5612ba546ae9ededbcffa3de18bb423a8e5f059f
Teresa <teresa.lenz@desy.de>
2018-10-29 21:16:27 +0100

    add all variables from config

--- DNN/GoF/produce_all_gof_inputs.py
+++ DNN/GoF/produce_all_gof_inputs.py
@@ -1,12 +1,30 @@
 import os
 
-variable_list = [ "pt_1" , "pt_2" , "m_vis" , "m_sv"]
+variable_list = [    "m_sv",
+                     "m_vis",
+                     "pt_vis",
+                     "pt_1",
+                     "pt_2",
+                     "jpt_2",
+                     "jm_1",
+                     "jm_2",
+                     "bpt_1",
+                     "bpt_2",
+                     "bcsv_1",
+                     "bcsv_2",
+                     "njets",
+                     "nbtag",
+                     "mt_1",
+                     "mt_2",
+                     "pt_tt",
+                     "mjj",
+                     "dijetpt",
+                     "met"]
 
 # Execute produceGoFInputs.C for all variables
 for var in variable_list :
+    print "****************************************************************************************"
+    print "****************************************************************************************"
     print "Execute " + var + "\n"
     cmd = "root -l -b -q produce_gof_input.cpp+\"(\\\"" + var+ "\\\")\""
-    
-    print "cmd  = " + cmd
-    
     os.system(cmd)
