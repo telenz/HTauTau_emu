b04a906346311c4d455d368a8ec527b6316e8eac
Teresa <teresa.lenz@desy.de>
2018-10-29 16:02:11 +0100

    debug

--- DNN/GoF/runGoF.sh
+++ DNN/GoF/runGoF.sh
@@ -1,24 +1,22 @@
 #!bin/bash
-MorphingSM2017 --base_path=/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/HTauTau_emu/DNN/GoF/output/  --real_data=true --jetfakes=0 --embedding=0 --postfix="-m_vis" --channel="em" --auto_rebin=true --stxs_signals="stxs_stage0" --categories="gof" --gof_category_name="em_inclusive" --era=2016 --output="2016_smhtt"
+
+# this script executes a gof test for a given variable $var
+ERA=2016
+DATACARD=${ERA}_workspace.root
+SEED=1234
+MASS=125
+NUM_TOYS=300
+var="m_vis"
 
 
-cd /nfs/dust/cms/user/mameyer/SM_HiggsTauTau/CombineHarvester/CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/
-make
+MorphingSM2017 --base_path=/nfs/dust/cms/user/tlenz/13TeV/2017/SM_HTauTau/HTauTau_emu/DNN/GoF/  --input_folder_em=output --real_data=true --jetfakes=0 --embedding=0 --postfix="-m_vis" --channel="em" --auto_rebin=true --stxs_signals="stxs_stage0" --categories="gof" --gof_category_name="em_inclusive" --era=2016 --output="2016_smhtt"
 
-cd /nfs/dust/cms/user/mameyer/SM_HiggsTauTau/CombineHarvester/CMSSW_7_4_7/src
-scram b -j8
 
 combineTool.py -M T2W -o workspace.root -i ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/bin/output/2016_smhtt/cmb/
 
 cp ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/bin/output/2016_smhtt/cmb/workspace.root 2016_workspace.root
 
 combine -M GoodnessOfFit --algo=saturated -m 125 -d 2016_workspace.root
-
-ERA=$1
-DATACARD=${ERA}_workspace.root
-SEED=1234
-MASS=125
-NUM_TOYS=300
 
 # Get test statistic value
 combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD
