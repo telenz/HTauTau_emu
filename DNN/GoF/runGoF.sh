f6bd40576bd75a56599a2c3143843477e08c3ada
Teresa <teresa.lenz@desy.de>
2018-10-29 19:57:52 +0100

    add mass as input argument + make script more flexible

--- DNN/GoF/runGoF.sh
+++ DNN/GoF/runGoF.sh
@@ -1,23 +1,29 @@
 #!bin/bash
 
-# this script executes a gof test for a given variable $var
+# Script which executes a gof test for a given variable $var
+
+# Pleas specify the following input variables
 ERA=2016
 DATACARD=${ERA}_workspace.root
 SEED=1234
 MASS=125
 NUM_TOYS=300
-var="m_vis"
+VAR="m_vis"
+BASE_PATH=/nfs/dust/cms/user/tlenz/13TeV/2017/SM_HTauTau/HTauTau_emu/DNN/GoF/
+INPUT_FOLDER=output
+OUTPUT_FOLDER=2016_smhtt
 
 # Produce the datacard for the em channel (please add the em channel in category gof in the morphing script)
-MorphingSM2017 --base_path=/nfs/dust/cms/user/tlenz/13TeV/2017/SM_HTauTau/HTauTau_emu/DNN/GoF/  --input_folder_em=output --real_data=true --jetfakes=0 --embedding=0 --postfix="-m_vis" --channel="em" --auto_rebin=true --stxs_signals="stxs_stage0" --categories="gof" --gof_category_name="em_inclusive" --era=2016 --output="2016_smhtt" --regional_jec=false --ggh_wg1=false
+MorphingSM2017 --base_path=$BASE_PATH  --input_folder_em=$INPUT_FOLDER --real_data=true --jetfakes=0 --embedding=0 --postfix="-$VAR" --channel="em" --auto_rebin=true --stxs_signals="stxs_stage0" --categories="gof" --gof_category_name="em_inclusive" --era=2016 --output=$OUTPUT_FOLDER --regional_jec=false --ggh_wg1=false
 
 # Create workspace
-combineTool.py -M T2W -o workspace.root -i ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/2016_smhtt/cmb/125/
+combineTool.py -M T2W -o workspace.root -m $MASS -i ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/2016_smhtt/cmb/125/
 
 # Rename workspace
-cp ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/2016_smhtt/cmb/125/workspace.root ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/2016_smhtt/cmb/125/2016_workspace.root
+workspace_location=${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/${OUTPUT_FOLDER}/cmb/125/
+cp ${workspace_location}/workspace.root ${workspacce_location}/2016_workspace.root
 
-cd ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/2016_smhtt/cmb/125/ 
+cd ${workspace_location}
 
 # Get test statistic value
 combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD
