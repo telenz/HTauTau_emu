fd3977e791462775ba4212156c9b73d99b1cc1af
Teresa <teresa.lenz@desy.de>
2018-10-30 17:35:23 +0100

    first full list of variables

--- DNN/GoF/produce_all_gof_inputs.py
+++ DNN/GoF/produce_all_gof_inputs.py
@@ -1,22 +1,41 @@
 import os
 
-# variable_list = [    "m_sv",
-#                      "m_vis",
-#                      "pt_1",
-#                      "pt_2",
-#                      "jpt_2",
-#                      "njets",
-#                      "nbtag",
-#                      "mt_1",
-#                      "mt_2",
-#                      "pt_tt",
-#                      "mjj",
-#                      "met"]
+variable_list = [ "m_sv",
+                  "m_vis",
+                  "pt_1",
+                  "pt_2",
+                  "eta_1",
+                  "eta_2",
+                  "jpt_1",
+                  "jpt_2",
+                  "njets",
+                  "nbtag",
+                  "mt_1",
+                  "mt_2",
+                  "pt_tt",
+                  "mjj",
+                  "met",
+                  "dzeta"
+                  ]
 
-variable_list = [ "jpt_2" ]
 
-axis_range = {"jpt_2" : [10,100] , "pt_2" : [10,0,200] , "m_vis" : [20,0,200]} 
-
+axis_range = { "m_sv"  : [30 , 300],
+               "m_vis" : [30 , 300],
+               "pt_1"  : [10 , 150],
+               "pt_2"  : [10 , 150],
+               "eta_1" : [-5 , +5],
+               "eta_2" : [-5 , +5],
+               "jpt_1" : [30 , 400],
+               "jpt_2" : [30 , 400],
+               "njets" : [0  , 8],
+               "nbtag" : [0  , 8],
+               "mt_1"  : [30 , 300],
+               "mt_2"  : [30 , 300],
+               "pt_tt" : [30 , 300],
+               "mjj"   : [30 , 300],
+               "met"   : [30 , 300],
+               "dzeta" : [-35 , 300],
+               }
 
 # Execute produce_gof_inputs.cpp for all variables
 
