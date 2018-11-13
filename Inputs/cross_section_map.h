bf43889c178050351718b28d02b112e0c1138b45
Teresa <teresa.lenz@desy.de>
2018-11-13 21:17:35 +0100

    updated with changes in filenames and new map process_map_2017

--- Inputs/cross_section_map.h
+++ Inputs/cross_section_map.h
@@ -2,45 +2,30 @@
 #define CROSS_SECTION_MAP_H
 
 // Define the subsamples that belong to a certain proccess
-const vector<TString> MuonEG_Run2017  = { "MuonEG_Run2017B" ,
-				    "MuonEG_Run2017C" ,
-				    "MuonEG_Run2017D" ,
-				    "MuonEG_Run2017E" ,
-				    "MuonEG_Run2017F" };
-const vector<TString> DYJets_2017     = { "DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" };
-const vector<TString> WJets_2017      = { "W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
-				    "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" };
-const vector<TString> TTbar_2017      = { "TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8" ,
-				    "TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8" ,
-				    "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8" };
-const vector<TString> SingleTop_2017  = { "ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8" ,
-				    "ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8" ,
-				    "ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8" ,
-				    "ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8" };
-const vector<TString> Diboson_2017    = { "WW_TuneCP5_13TeV-pythia8" ,
-				    "WZ_TuneCP5_13TeV-pythia8" ,
-				    "ZZ_TuneCP5_13TeV-pythia8" };
-const vector<TString> GluGluHToTauTau_2017 = { "GluGluHToTauTau_M125" };
-const vector<TString> VBFHToTauTau_2017    = { "VBFHToTauTau_M125" };
-
-const map< TString , vector<TString> > samples_map = {
-  { "MuonEG_Run2017_dnn_" + channel + "_" + version , MuonEG_Run2017 },
-  { "DYJets_dnn_"         + channel + "_" + version , DYJets },
-  { "WJets_dnn_"          + channel + "_" + version , WJets },
-  { "TTbar_dnn_"          + channel + "_" + version , TTbar },
-  { "SingleTop_dnn_"      + channel + "_" + version , SingleTop },
-  { "Diboson_dnn_"        + channel + "_" + version , Diboson },
-  { "ggH_dnn_"            + channel + "_" + version , GluGluHToTauTau },
-  { "VBFH_dnn_"           + channel + "_" + version , VBFHToTauTau }
-};
-
+const vector<TString> MuonEG_Run2017       = { "MuonEG_Run2017B" ,
+					       "MuonEG_Run2017CtoF"};
+const vector<TString> DYJets_2017          = { "DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" };
+const vector<TString> WJets_2017           = { "W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" ,
+					       "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" };
+const vector<TString> TTbar_2017           = { "TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8" ,
+					       "TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8" ,
+					       "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8" };
+const vector<TString> SingleTop_2017       = { "ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8" ,
+					       "ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8" ,
+					       "ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8" ,
+					       "ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8" };
+const vector<TString> Diboson_2017         = { "WW_TuneCP5_13TeV-pythia8" ,
+					       "WZ_TuneCP5_13TeV-pythia8" ,
+					       "ZZ_TuneCP5_13TeV-pythia8" };
+const vector<TString> GluGluHToTauTau_2017 = { "GluGluHToTauTau_M125_13TeV_powheg_pythia8" };
+const vector<TString> VBFHToTauTau_2017    = { "VBFHToTauTau_M125_13TeV_powheg_pythia8" };
 
 
 // Cross-section map ( needs to be checked again )
@@ -66,9 +51,38 @@ const map<TString, double> xsec_map_2017 = {
   { "WW_TuneCP5_13TeV-pythia8" , 49.997 },
   { "WZ_TuneCP5_13TeV-pythia8" , 5.595 },
   { "ZZ_TuneCP5_13TeV-pythia8" , 10.71 },
-  { "GluGluHToTauTau_M125"     , 48.58*0.0627 },
-  { "VBFHToTauTau_M125"        , 3.782*0.0627 }
+  { "GluGluHToTauTau_M125_13TeV_powheg_pythia8" , 48.58*0.0627 },
+  { "VBFHToTauTau_M125_13TeV_powheg_pythia8"    , 3.782*0.0627 }
 };
+
+
+const map<TString , TString> process_map_2017 = {
+  { "WJets"   , "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "W1Jets"  , "W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "W2Jets"  , "W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "W3Jets"  , "W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "W4Jets"  , "W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "DYJets"  , "DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "DY1Jets" , "DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "DY2Jets" , "DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "DY3Jets" , "DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"},
+  { "DY4Jets" , "DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"},
+};
+
+
+double getNEventsProcessed(TString filename)
+{
+  TFile * file = new TFile(filename);
+  TH1D * histWeightsH = (TH1D*)file->Get("histWeightsH");
+  if(!histWeightsH){
+    cout << endl << endl << "Histogram histWeightsH doesn't exist in the file "<< filename <<". Quit program." << endl << endl;
+    exit(-1);
+  }
+  double nevents = histWeightsH->GetSumOfWeights();
+  file -> Close();
+  delete file;
+  return nevents;
+}
 
 
 #endif
