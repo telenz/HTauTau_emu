d70c74d0e6cccdea333d5eb6c19f268e6549e864
Teresa <teresa.lenz@desy.de>
2018-11-14 11:03:54 +0100

    now in alphabetical order

--- DNN/GoF/make_pvalues_list.py
+++ DNN/GoF/make_pvalues_list.py
@@ -1,17 +1,18 @@
 import os
+from glob import glob
 
-input_dir = "output/var_1d/"
+input_dir = "output/var_1d_correct_qcd_estimation"
 
+files_list = glob(os.path.join(input_dir, '*.json'))
+print files_list
+for filename in sorted(files_list):
 
-for filename in os.listdir(input_dir) :
-    if not filename.endswith(".json") :
-        continue
     var = filename.split("gof-")[1]
     var = var.split(".json")[0]
-    f = open(input_dir + filename , 'r')
-    lines=f.readlines()
 
-    for line in lines:
+    f = open(filename , 'r')
+    lines=f.readlines()
+    for line in lines:     
         if not '\"p\"' in line:
             continue
         pvalue = line
