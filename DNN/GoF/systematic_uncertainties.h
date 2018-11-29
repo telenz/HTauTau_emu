5b77590983e0060457795965f95fbb64ec15fa3e
Teresa <teresa.lenz@desy.de>
2018-11-29 14:06:06 +0100

    first commit

---
+++ DNN/GoF/systematic_uncertainties.h
@@ -0,0 +1,38 @@
+#include "useful_classes.h"
+
+Sample create_systematic_uncertainty( TString uncertainty_name, TString combine_harvester_name,
+				      bool gof_2d,
+				      Sample mother_sample, TTree* input_tree){
+
+  Sample uncertainty_sample = mother_sample;
+  TString variable = "";
+  if(!gof_2d) variable = mother_sample.variable_1d;
+  else        variable = mother_sample.variable_2d;
+
+  mother_sample.uncertainties.insert( make_pair( uncertainty_name , uncertainty_sample ) );
+  mother_sample.uncertainties[uncertainty_name].name += combine_harvester_name;
+  if(input_tree -> GetBranch("dzeta_" +uncertainty_name)) mother_sample.uncertainties[uncertainty_name].cutString.ReplaceAll("dzeta","dzeta_"+uncertainty_name);
+  if(input_tree -> GetBranch("pt_1_" +uncertainty_name)) mother_sample.uncertainties[uncertainty_name].cutString.ReplaceAll("pt_1","pt_1_"+uncertainty_name);
+  if(input_tree -> GetBranch("pt_2_" +uncertainty_name)) mother_sample.uncertainties[uncertainty_name].cutString.ReplaceAll("pt_2","pt_2_"+uncertainty_name);
+  if(input_tree -> GetBranch("mTdileptonMET_" +uncertainty_name)) mother_sample.uncertainties[uncertainty_name].cutString.ReplaceAll("mTdileptonMET","mTdileptonMET_"+uncertainty_name);
+  if(input_tree -> GetBranch("nbtag_" +uncertainty_name)) mother_sample.uncertainties[uncertainty_name].cutString.ReplaceAll("nbtag","nbtag_"+uncertainty_name);
+  if(!gof_2d){
+    if(input_tree -> GetBranch( variable + "_" + uncertainty_name )) mother_sample.uncertainties[uncertainty_name].variable_1d = variable + "_" + uncertainty_name;
+    else cout<<"No systematic shift for "<<uncertainty_name<<" for variable "<<variable<<" available in tree."<<endl;
+  }
+  else{
+
+    // split the two variables into two seperate strings
+    TString var1, var1_variation, var2, var2_variation;
+    var1 = mother_sample.variable_2d( 0 , mother_sample.variable_2d.First(":") );
+    var2 = mother_sample.variable_2d( mother_sample.variable_2d.First(":")+1 , mother_sample.variable_2d.Length() );
+    var1_variation   = var1;
+    var2_variation   = var2;
+
+    if(input_tree -> GetBranch(var1_variation+"_"+uncertainty_name)) var1_variation = var1 + "_" + uncertainty_name;
+    if(input_tree -> GetBranch(var2_variation+"_"+uncertainty_name)) var2_variation = var2 + "_" + uncertainty_name;
+    mother_sample.uncertainties[uncertainty_name].variable_2d = var1_variation   + ":" + var2_variation;
+  }
+  return  mother_sample;
+}
+
