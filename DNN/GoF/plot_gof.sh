29525b6b6878b6ca89a8a17f159f750cf8490ad1
Mareike Meyer <mareike.meyer@desy.de>
2018-12-07 09:07:35 +0100

    added full path

--- DNN/GoF/plot_gof.sh
+++ DNN/GoF/plot_gof.sh
@@ -2,4 +2,4 @@
 
 VARIABLES=variables.yaml
 
-python plot_gof.py $VARIABLES 'output/2016/var_2d' 'em' '2016'
+python plot_gof.py $VARIABLES '/nfs/dust/cms/user/tlenz/13TeV/2017/SM_HTauTau/HTauTau_emu/DNN/GoF/output/2017/var_2d' 'em' '2017'
