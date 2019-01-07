3deb646a5fd29019164ec5688c325933d852181c
Mareike Meyer <mareike.meyer@desy.de>
2019-01-07 13:52:50 +0100

    added instructions for setting up Combine with CMMSW_8_1_0

---
+++ Setup/setup_cmssw_combineharvester_810.sh
@@ -0,0 +1,21 @@
+export SCRAM_ARCH=slc6_amd64_gcc530
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+scram project CMSSW CMSSW_8_1_0
+cd CMSSW_8_1_0/src
+
+# Clone combine
+git clone ssh://git@github.com/cms-analysis/HiggsAnalysis-CombinedLimit HiggsAnalysis/CombinedLimit
+cd HiggsAnalysis/CombinedLimit
+git fetch origin
+git checkout v7.0.12
+cd ../..
+
+# Clone CombineHarvester
+git clone ssh://git@github.com/cms-analysis/CombineHarvester CombineHarvester -b SMHTT2017-dev
+mkdir -p CombineHarvester/HTTSM2017/shapes
+
+# Build
+scram b -j 24
+scram b python
