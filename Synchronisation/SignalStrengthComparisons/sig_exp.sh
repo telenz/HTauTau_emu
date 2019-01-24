d4aff91e802744ac2943bb97e085be788a7e19e2
Mareike Meyer <mareike.meyer@desy.de>
2019-01-24 14:04:44 +0100

    corrected bugs

--- Synchronisation/SignalStrengthComparisons/sig_exp.sh
+++ Synchronisation/SignalStrengthComparisons/sig_exp.sh
@@ -8,9 +8,6 @@ rm -f ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/${ERA}_smhtt/cmb/125/$
 
 combineTool.py -M T2W -o ${ERA}_workspace.root -i output/${ERA}_smhtt/cmb/125   \
              --parallel 8 -m 125  \
-             -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel  \
-             --PO '"map=^.*/ggH.?$:r_ggH[1,-5,5]"' \
-             --PO '"map=^.*/qqH.?$:r_qqH[1,-5,5]"'
-
+          
 combine -M Significance -t -1 --expectSignal 1 --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 --significance -m 125 -n $ERA --toysFrequentist output/${ERA}_smhtt/cmb/125/${ERA}_workspace.root
 
