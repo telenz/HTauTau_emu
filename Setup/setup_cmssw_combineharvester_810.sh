7cfa47d74409c2921107a3bb9ef72a78047ebe71
Teresa <teresa.lenz@desy.de>
2019-02-04 12:46:08 +0100

    updated setup for 8_1_0 with https: instead of ssh:

--- Setup/setup_cmssw_combineharvester_810.sh
+++ Setup/setup_cmssw_combineharvester_810.sh
@@ -2,23 +2,27 @@ export SCRAM_ARCH=slc6_amd64_gcc530
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
+export SCRAM_ARCH=slc6_amd64_gcc530
 scram project CMSSW CMSSW_8_1_0
 cd CMSSW_8_1_0/src
+cmsenv
 
-# Clone combine
-git clone ssh://git@github.com/cms-analysis/HiggsAnalysis-CombinedLimit HiggsAnalysis/CombinedLimit
+git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 cd HiggsAnalysis/CombinedLimit
 git fetch origin
 git checkout v7.0.12
 cd ../..
 
-# Clone CombineHarvester
-git clone ssh://git@github.com/cms-analysis/CombineHarvester CombineHarvester -b SMHTT2017-dev
-mkdir -p CombineHarvester/HTTSM2017/shapes
+git clone https://github.com/cms-analysis/CombineHarvester.git CombineHarvester
+cd CombineHarvester
+git checkout SMHTT2017-dev
+cd -
+
+cd CombineHarvester/HTTSM2017
+git clone https://:@gitlab.cern.ch:8443/cms-htt/SM-ML-2017.git shapes
+cd -
 
 # Build
 scram b -j 24
 scram b python
 
-mkdir CombineHarvester/HTTSM2017/output
-cd -
