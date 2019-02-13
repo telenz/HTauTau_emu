cf3a67ba2fa2043c1755d2149f1cf199ba0b886f
Mareike Meyer <mareike.meyer@desy.de>
2019-02-13 09:31:14 +0100

    added PreFit root file

--- DNN/Datacards/make_post-fit.sh
+++ DNN/Datacards/make_post-fit.sh
@@ -1,4 +1,4 @@
-CMSSW_LOCATION=/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/CombineHarvester/2017/CMSSW_8_1_0/src
+CMSSW_LOCATION=/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/CombineForUnbliding/CMSSW_8_1_0/src
 DATACARD_PATH=${CMSSW_LOCATION}/CombineHarvester/HTTSM2017/output/${ERA}_smhtt/cmb/125/
 BASE_PATH=/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/HTauTau_emu/DNN/Datacards/
 
@@ -10,9 +10,14 @@ cmsenv
 cd ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/
 
 PostFitShapesFromWorkspace -m 125 -w ${DATACARD_PATH}/${ERA}_workspace.root \
+    -d output/${ERA}_smhtt/cmb/125/combined.txt.cmb -o ${ERA}_datacard_shapes_prefit.root
+
+PostFitShapesFromWorkspace -m 125 -w ${DATACARD_PATH}/${ERA}_workspace.root \
     -d output/${ERA}_smhtt/cmb/125/combined.txt.cmb -o ${ERA}_datacard_shapes_postfit_sb.root \
     -f fitDiagnostics${ERA}.root:fit_s --postfit
 
 cd ${CURRENT_PATH}
 
 cp ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/${ERA}_datacard_shapes_postfit_sb.root ${BASE_PATH}/output/${ERA}
+cp ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/${ERA}_datacard_shapes_prefit.root ${BASE_PATH}/output/${ERA}
+
