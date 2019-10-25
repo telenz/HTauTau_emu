e8e82418ab6c96f16ce2fc2a2049703244db946c
Teresa <teresa.lenz@desy.de>
2019-10-25 15:00:48 +0200

    changes such that stack is plotted

--- Plotting/plot_data_and_mc.py
+++ Plotting/plot_data_and_mc.py
@@ -9,7 +9,7 @@ rt.gROOT.SetBatch(rt.kTRUE)
 variable_list = [  "pt_1"  ]
 axis_range = {"pt_1" : [10,30,100] }
 
-# open tree
+# Open trees
 input_dir = "/nfs/dust/cms/user/tlenz/13TeV/2018/SM_HTauTau/HTauTau_emu/Inputs/NTuples_2017/"
 f0,tree_Data   = f.openTree(input_dir+"/em-NOMINAL_ntuple_MuonEG.root","TauCheck")
 f1,tree_DYJets = f.openTree(input_dir+"/em-NOMINAL_ntuple_DYJets.root","TauCheck")
@@ -37,11 +37,9 @@ for var in variable_list :
     h_Data = f.InitHist(h_Data, var, 1);
     h_ZTT  = f.InitHist(h_ZTT , var, rt.TColor.GetColor("#FFCC66"));
 
-    # Draw
-    upper_pad = rt.TPad("upper", "pad",0,0.19,1,1)
-    upper_pad.cd();
-    tree_Data.Draw(   var + " >> "+h_Data.GetName(), "("+selection+")"                 , "e1" )
-    tree_DYJets.Draw( var + " >> h_ZTT_"+var       , weight+"*("+selection+" && isZTT)", "hist same" )
+    # Draw events from tree to histogram according to defined selection
+    tree_Data.Draw(   var + " >> "+h_Data.GetName(), "("+selection+")"                  )
+    tree_DYJets.Draw( var + " >> h_ZTT_"+var       , weight+"*("+selection+" && isZTT)" )
     print "data = " + str(h_Data.GetSumOfWeights())
     print "ZTT  = " + str(h_ZTT.GetSumOfWeights())
     print "Bkg  = " + str(h_ZTT.GetSumOfWeights() )
@@ -50,13 +48,20 @@ for var in variable_list :
     bkg = rt.THStack("Background","");
     bkg.Add(h_ZTT);
 
+    # Draw the upper pad
+    upper_pad = rt.TPad("upper", "pad",0,0.31,1,1)
+    upper_pad.cd();
+    h_Data.Draw("e1")
+    bkg.Draw("hist same")
+
     # Make a legend to the plot
     legend = rt.TLegend(0.65,0.7,0.89,0.92);
     legend.SetTextFont(42);
     legend.AddEntry(h_Data, "Observed", "ple");
     legend.AddEntry(h_ZTT,"Z#rightarrow #tau#tau","f");
+    legend.Draw("same")
 
-    # Make a ratio plot
+    # Make a ratio plot and draw it to lower pad
     lower_pad = rt.TPad("lower", "pad",0,0,1,0.31)
     lower_pad.cd()
     histo_ratio = h_Data.Clone("ratioH");
@@ -66,6 +71,7 @@ for var in variable_list :
     histo_ratio.GetXaxis().SetTitle(var);
     histo_ratio.Draw("same")
 
+    # Draw both pads to canvas
     c.cd()
     upper_pad.Draw()
     lower_pad.Draw("same")
@@ -76,6 +82,3 @@ for var in variable_list :
     # Clean gDirectory
     rt.gDirectory.Delete("h1")
     rt.gDirectory.Delete("h2")
-
-
-
