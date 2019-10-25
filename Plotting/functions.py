a47514b68ce0119e0c53b529e1db39b08070f081
Teresa <teresa.lenz@desy.de>
2019-10-25 14:43:32 +0200

    add comments and change logic of ratio plot + style changes

--- Plotting/functions.py
+++ Plotting/functions.py
@@ -5,16 +5,8 @@ def openTree(filename,treename):
     tree = f1.Get(treename)
     return (f1,tree)
 
-def createH1(xaxisname , nbins, xmin, xmax):
-    h1 = rt.TH1D("h_"+xaxisname,"h_"+xaxisname, nbins , xmin, xmax)
-    h1.SetLineColor(1)
-    h1.SetLineWidth(3)
-    h1.GetYaxis().SetTitleSize(20)
-    h1.GetYaxis().SetTitleFont(43)
-    h1.GetYaxis().SetTitleOffset(1.55)
-    h1.GetXaxis().SetTitle(xaxisname)
-    h1.SetTitle("")
-    h1.SetStats(0)
+def createH1(xaxis_name , nbins, xmin, xmax):
+    h1 = rt.TH1D("h_"+xaxis_name,"h_"+xaxis_name, nbins , xmin, xmax)
     return h1
 
 
@@ -35,14 +27,35 @@ def hist2Lists(hist) :
 
     return list_x , list_y , list_yerr
 
-def InitHist(hist, color) :
+def InitHist(hist, xaxis_name, color=1) :
+
     hist.SetFillColor(color);
     hist.SetFillStyle(1001);
     hist.SetLineColor(1);
     hist.SetLineStyle(1);
     hist.SetLineWidth(2);
+
     hist.SetMarkerColor(1);
-    hist.SetMarkerStyle(21);
-    hist.SetMarkerSize(0.0);
-    # hist.SetStats(rt.kFalse);
+    hist.SetMarkerStyle(20);
+    hist.SetMarkerSize(1.0);
+
+    hist.GetXaxis().SetLabelFont(43)
+    hist.GetYaxis().SetLabelFont(43)
+    hist.GetYaxis().SetTitleFont(43)
+    hist.GetYaxis().SetTitleFont(43)
+
+    hist.GetXaxis().SetLabelSize(20)
+    hist.GetYaxis().SetLabelSize(20)
+    hist.GetXaxis().SetTitleSize(20)
+    hist.GetYaxis().SetTitleSize(20)
+
+    hist.GetYaxis().SetTitleOffset(1.55)
+    hist.GetYaxis().SetTitleOffset(1.55)
+
+    hist.GetXaxis().SetTitle(xaxis_name)
+
+    hist.SetTitle("")
+
+    hist.SetStats(0)
+
     return hist
--- Plotting/plot_data_and_mc.py
+++ Plotting/plot_data_and_mc.py
@@ -33,30 +33,47 @@ for var in variable_list :
     h_Data = f.createH1(var,nbins,xmin,xmax)
     h_ZTT  = h_Data.Clone( "h_ZTT_"+var)
 
-    h_ZTT = f.InitHist(h_ZTT,rt.TColor.GetColor("#FFCC66"));
+    # Specify a few settings
+    h_Data = f.InitHist(h_Data, var, 1);
+    h_ZTT  = f.InitHist(h_ZTT , var, rt.TColor.GetColor("#FFCC66"));
 
     # Draw
-    tree_Data.Draw( var + " >> "+h_Data.GetName(),"("+selection+")","hist" )
-    tree_DYJets.Draw( var + " >> h_ZTT_"+var , weight+"*("+selection+" && isZTT)","hist" )
+    upper_pad = rt.TPad("upper", "pad",0,0.19,1,1)
+    upper_pad.cd();
+    tree_Data.Draw(   var + " >> "+h_Data.GetName(), "("+selection+")"                 , "e1" )
+    tree_DYJets.Draw( var + " >> h_ZTT_"+var       , weight+"*("+selection+" && isZTT)", "hist same" )
     print "data = " + str(h_Data.GetSumOfWeights())
     print "ZTT  = " + str(h_ZTT.GetSumOfWeights())
     print "Bkg  = " + str(h_ZTT.GetSumOfWeights() )
 
+    # Create a stack with all backgrounds
     bkg = rt.THStack("Background","");
     bkg.Add(h_ZTT);
 
+    # Make a legend to the plot
     legend = rt.TLegend(0.65,0.7,0.89,0.92);
     legend.SetTextFont(42);
     legend.AddEntry(h_Data, "Observed", "ple");
     legend.AddEntry(h_ZTT,"Z#rightarrow #tau#tau","f");
 
-    rp = rt.TRatioPlot(bkg , h_Data);
-    rp.Draw();
-    legend.Draw("same")
+    # Make a ratio plot
+    lower_pad = rt.TPad("lower", "pad",0,0,1,0.31)
+    lower_pad.cd()
+    histo_ratio = h_Data.Clone("ratioH");
+    histo_ratio = f.InitHist(histo_ratio, var, 1)
+    histo_ratio.Divide(bkg.GetStack().Last())
+    histo_ratio.GetYaxis().SetRangeUser(0.0,4.0);
+    histo_ratio.GetXaxis().SetTitle(var);
+    histo_ratio.Draw("same")
 
-    # save plot
+    c.cd()
+    upper_pad.Draw()
+    lower_pad.Draw("same")
+
+    # Save plot
     c.Print("figures/"+var+".pdf")    
 
+    # Clean gDirectory
     rt.gDirectory.Delete("h1")
     rt.gDirectory.Delete("h2")
 
