88f2fb1c58ddbaf72ed8d8e28f042f1761bc0750
Mareike Meyer <mareike.meyer@desy.de>
2019-12-10 17:12:36 +0100

    compared cross sections to AN, added number of events for 2016

--- Inputs/settings_for_eras.h
+++ Inputs/settings_for_eras.h
@@ -94,11 +94,12 @@ const vector<TString> ttH_2017             = { "ttHToTauTau_M125_TuneCP5_13TeV-p
 // 2016
 const vector<TString> MuonEG_Run2016       = { "MuonEG_Run2016B" , "MuonEG_Run2016C" , "MuonEG_Run2016D" , "MuonEG_Run2016E" , "MuonEG_Run2016F" , "MuonEG_Run2016G" , "MuonEG_Run2016H" };
 const vector<TString> Embedded_2016        = { "Embedding_Run2016" };
-const vector<TString> DYJets_2016          = { "DY1JetsToLL_M-50" , "DY2JetsToLL_M-50" , "DY3JetsToLL_M-50" , "DY4JetsToLL_M-50" , "DYJetsToLL_M-50" , "DYJetsToLL_M-10to50" , "EWKZ2Jets" };
+const vector<TString> DYJets_2016          = { "DY1JetsToLL_M-50" , "DY2JetsToLL_M-50" , "DY3JetsToLL_M-50" , "DY4JetsToLL_M-50" , "DYJetsToLL_M-50" , "DYJetsToLL_M-10to50" , "EWKZ2Jets_ZToLL", "EWKZ2Jets_ZToNuNu" };
 const vector<TString> WJets_2016           = { "W1JetsToLNu" , "W2JetsToLNu" , "W3JetsToLNu" , "W4JetsToLNu" , "WJetsToLNu" , "WGToLNuG" , "WGstarToLNuEE" , "WGstarToLNuMuMu" , "EWKWPlus2Jets" , "EWKWMinus2Jet" };
 const vector<TString> TTbar_2016           = { "TTbar" };
 const vector<TString> SingleTop_2016       = { "ST_t-channel_antitop" , "ST_t-channel_top" , "ST_tW_antitop" , "ST_tW_top" };
-const vector<TString> Diboson_2016         = { "VVTo2L2Nu" , "WZJToLLLNu" , "WZTo1L1Nu2Q" , "WZTo1L3Nu" , "WZTo2L2Q" , "ZZTo2L2Q" , "ZZTo4L" , "WWToLNuQQ" };
+//const vector<TString> Diboson_2016         = { "VVTo2L2Nu" , "WZJToLLLNu" , "WZTo1L1Nu2Q" , "WZTo1L3Nu" , "WZTo2L2Q" , "ZZTo2L2Q" , "ZZTo4L" , "WWToLNuQQ" };
+const vector<TString> Diboson_2016         = { "WW", "WZ", "ZZ" };
 const vector<TString> GluGluHToTauTau_2016 = { "GluGluHToTauTau_M125" };
 const vector<TString> VBFHToTauTau_2016    = { "VBFHToTauTau_M125" , "ZHToTauTau_M125" , "WplusHToTauTau_M125" , "WminusHToTauTau_M125" };
 const vector<TString> ZHToTauTau_2016      = { "ZHToTauTau_M125" };
@@ -110,21 +111,21 @@ const vector<TString> ttH_2016             = { "ttHJetToTT_M125" };
 // **************************************************************************************************
 // **************************************************************************************************
 // Cross-section map
-// 2018 (taken from 2017 - 2017 was checked)
+// 2018 (checked with AN-19-177)
 const map<TString, double> xsec_map_2018 = {
    { "DYJetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8"  , 6077.22 }, // NNLO xsec -> k-factor=1.1137 (see mail from Izaak on tauid hn 14.10.19)
-   { "DY1JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 877.8*1.137 },
-   { "DY2JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 304.4*1.137 },
-   { "DY3JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 111.5*1.137 },
-   { "DY4JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 44.04*1.137 },
+   { "DY1JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 877.8*1.165 },
+   { "DY2JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 304.4*1.165 },
+   { "DY3JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 111.5*1.165 },
+   { "DY4JetsToLL_M-50_TuneCP5_13TeV_madgraphMLM_pythia8" , 44.03*1.165 },
    { "EWKZ2Jets_ZToNuNu_TuneCP5_PSweights_13TeV-madgraph-pythia8" , 10.66 },
    { "EWKZ2Jets_ZToLL_M-50_TuneCP5_PSweights_13TeV-madgraph-pythia8" , 4.321 },
    { "DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8" , 21658.0 },
    { "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"  , 61526.7 },   // NNLO -> k-factor=1.162
-   { "W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 8104.0*1.162 },
-   { "W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 2793.0*1.162 },
-   { "W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 992.5*1.162 },
-   { "W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 544.3*1.162 },
+   { "W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 8104.0*1.079 },
+   { "W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 2793.0*1.079 },
+   { "W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 992.5*1.079 },
+   { "W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 544.3*1.079 },
    { "EWKWMinus2Jets_WToLNu_M-50_TuneCP5_13TeV-madgraph-pythia8" , 23.24},
    { "EWKWPlus2Jets_WToLNu_M-50_TuneCP5_13TeV-madgraph-pythia8" , 29.59},
    { "WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8" , 464.4},
@@ -138,30 +139,30 @@ const map<TString, double> xsec_map_2018 = {
    { "WW_TuneCP5_13TeV-pythia8" , 118.7 },  // NNLO
    { "WZ_TuneCP5_13TeV-pythia8" , 27.57 },
    { "ZZ_TuneCP5_13TeV-pythia8" , 12.14 },
-   { "GluGluHToTauTau_M125_13TeV_powheg_pythia8" , 48.58*0.0627 },
+   { "GluGluHToTauTau_M125_13TeV_powheg_pythia8" , 3.08 }, //bottom quark associated production with 1% of the gluon fusion cross section is included in ggh
    { "VBFHToTauTau_M125_13TeV_powheg_pythia8" , 3.782*0.0627 },
    { "ZHToTauTau_M125_13TeV_powheg_pythia8" , 0.0554},
    { "WplusHToTauTau_M125_13TeV_powheg_pythia8" , 0.0527 },
-   { "WminusHToTauTau_M125_13TeV_powheg_pythia8" , 0.0358 },
+   { "WminusHToTauTau_M125_13TeV_powheg_pythia8" , 0.0334 },
    { "GluGluHToWWTo2L2Nu_M125_13TeV_powheg2_JHUGenV714_pythia8" , 1.091343 }, // 48.58 (Production) * 0.214 (H->WW) * 0.104976 (WW->2l2Nu), from Dennis Roy (Heavy H->WW analysis), SM H->WW analysis still uses old ggh cross section (according to their AN), other values are the same
    { "VBFHToWWTo2L2Nu_M125_13TeV_powheg2_JHUGenV714_pythia8" , 0.084962 },// 3.782 * 0.214 * 0.104976, from Dennis Roy (Heavy H->WW analysis)
    { "ttHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8" , 0.5071*0.0627 },
 };
-// 2017 (checked again - reference is http://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2018/255 -> DY xsec updated )
+// 2017 (checked again - AN-19-177 -> DY xsec updated )
 const map<TString, double> xsec_map_2017 = {
   { "DYJetsToLL_M-10to50_13TeV-12Apr2018"      , 21658.0 },
   { "DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"  , 6077.22 }, // NNLO xsec -> k-factor=1.137 (see mail from Izaak on tauid hn 14.10.19)
-  { "DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 877.8*1.137 },
-  { "DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 304.4*1.137 },
-  { "DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 111.5*1.137 },
-  { "DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 44.04*1.137 },
+  { "DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 877.8*1.165 },
+  { "DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 304.4*1.165 },
+  { "DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 111.5*1.165 },
+  { "DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8" , 44.04*1.165 },
   { "EWKZ2Jets_ZToLL_M-50_TuneCP5_13TeV-madgraph-pythia8" , 4.321 },
   { "EWKZ2Jets_ZToNuNu_TuneCP5_13TeV-madgraph-pythia8" , 10.66 },
   { "WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"  , 61526.7 },   // NNLO -> k-factor=1.162
-  { "W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 8104.0*1.162 },
-  { "W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 2793.0*1.162 },
-  { "W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 992.5*1.162 },
-  { "W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 544.3*1.162 },
+  { "W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 8104.0*1.079 },
+  { "W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 2793.0*1.079 },
+  { "W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 992.5*1.079 },
+  { "W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8" , 544.3*1.079 },
   { "WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8"    , 464.4 },
   { "EWKWMinus2Jets_WToLNu_M-50_TuneCP5_13TeV-madgraph-pythia8" , 23.24 },
   { "EWKWPlus2Jets_WToLNu_M-50_TuneCP5_13TeV-madgraph-pythia8" , 29.59 },
@@ -175,53 +176,57 @@ const map<TString, double> xsec_map_2017 = {
   { "WW_TuneCP5_13TeV-pythia8" , 118.7 },  // NNLO
   { "WZ_TuneCP5_13TeV-pythia8" , 27.57 },
   { "ZZ_TuneCP5_13TeV-pythia8" , 12.14 },
-  { "GluGluHToTauTau_M125_13TeV_powheg_pythia8" , 48.58*0.0627 },
+  { "GluGluHToTauTau_M125_13TeV_powheg_pythia8" , 3.08 }, //bottom quark associated production with 1% of the gluon fusion cross section is included in ggh
   { "VBFHToTauTau_M125_13TeV_powheg_pythia8"    , 3.782*0.0627 },
   { "ZHToTauTau_M125_13TeV_powheg_pythia8"      , 0.0554 },
   { "WplusHToTauTau_M125_13TeV_powheg_pythia8"  , 0.0527 },
-  { "WminusHToTauTau_M125_13TeV_powheg_pythia8" , 0.0358 },
+  { "WminusHToTauTau_M125_13TeV_powheg_pythia8" , 0.0334 },
   { "GluGluHToWWTo2L2Nu"                        , 1.091343 },  // see 2018
   { "VBFHToWWTo2L2Nu_M125_13TeV_powheg2_JHUGenV714_pythia8" , 0.084962 }, // see 2018
   { "ttHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8"         , 0.5071*0.0627 }  // from: see links above
 };
 
-// 2016 (taken from AN2016_355_v10 with minor unrelevant deviations - everything was checked)
+// 2016 (taken from AN-19-177)
 const map<TString, double> xsec_map_2016 = {
-    { "DYJetsToLL_M-10to50"      , 18610 },
+    { "DYJetsToLL_M-10to50"      , 21658.0 },
     { "DYJetsToLL_M-50"          , 6077.22 }, // NNLO xsec -> k-factor=1.227 (see mail from Izaak on tauid hn 14.10.19)
-    { "DY1JetsToLL_M-50"         , 1.227*1012.5 },
-    { "DY2JetsToLL_M-50"         , 1.227*332.8 },
-    { "DY3JetsToLL_M-50"         , 1.227*101.8 },
-    { "DY4JetsToLL_M-50"         , 1.227*54.8 },
+    { "DY1JetsToLL_M-50"         , 1.227*1012.5 },// to do: check!
+    { "DY2JetsToLL_M-50"         , 1.227*332.8 },// to do: check!
+    { "DY3JetsToLL_M-50"         , 1.227*101.8 },// to do: check!
+    { "DY4JetsToLL_M-50"         , 1.227*54.8 },// to do: check!
     { "WJetsToLNu"               , 61526.7 },
-    { "W1JetsToLNu"              , 1.221*9644.5 },
-    { "W2JetsToLNu"              , 1.221*3144.5 },
-    { "W3JetsToLNu"              , 1.221*954.8 },
-    { "W4JetsToLNu"              , 1.221*485.6 },
-    { "TTbar"                    , 831.76 },
+    { "W1JetsToLNu"              , 1.221*9644.5 },// to do: check!
+    { "W2JetsToLNu"              , 1.221*3144.5 },// to do: check!
+    { "W3JetsToLNu"              , 1.221*954.8 },// to do: check!
+    { "W4JetsToLNu"              , 1.221*485.6 },// to do: check!
+    { "TTbar"                    , 831.6 },
     { "ST_t-channel_antitop"     , 80.95 },
     { "ST_t-channel_top"         , 136.02 },
-    { "ST_tW_antitop"            , 35.6 },
-    { "ST_tW_top"                , 35.6 },
-    { "VVTo2L2Nu"                , 11.95 },
-    { "WWToLNuQQ"                , 49.997 },
-    { "WZTo2L2Q"                 , 5.595 },
-    { "WZTo1L1Nu2Q"              , 10.71 },
-    { "WZTo1L3Nu"                , 3.05 },
-    { "WZJToLLLNu"               , 5.26 },
-    { "ZZTo4L"                   , 1.212 },
-    { "ZZTo2L2Q"                 , 3.22 },
+    { "ST_tW_antitop"            , 35.85 },
+    { "ST_tW_top"                , 35.85 },
+    // { "VVTo2L2Nu"                , 11.95 },
+    // { "WWToLNuQQ"                , 49.997 },
+    // { "WZTo2L2Q"                 , 5.595 },
+    // { "WZTo1L1Nu2Q"              , 10.71 },
+    // { "WZTo1L3Nu"                , 3.05 },
+    // { "WZJToLLLNu"               , 5.26 },
+    // { "ZZTo4L"                   , 1.212 },
+    // { "ZZTo2L2Q"                 , 3.22 },
+    {"WW", 118.7},
+    {"WZ", 27.57},
+    {"ZZ", 12.14},
     { "WGToLNuG"                 , 178.4 }, // xsdb
     { "WGstarToLNuMuMu"          , 2.793 },
     { "WGstarToLNuEE"            , 3.526 },
-    { "EWKWPlus2Jets"            , 25.62 },
-    { "EWKWMinus2Jet"            , 20.20 },
-    { "EWKZ2Jets"                , 3.987 },
-    { "GluGluHToTauTau_M125"     , 48.58*0.0627 },
+    { "EWKWPlus2Jets"            , 29.59 },
+    { "EWKWMinus2Jet"            , 23.24 },
+    { "EWKZ2Jets_ZToLL"          , 4.321 },
+    { "EWKZ2Jets_ZToNuNu"        , 10.66 },
+    { "GluGluHToTauTau_M125"     , 3.08 },//bottom quark associated production with 1% of the gluon fusion cross section is included in ggh
     { "VBFHToTauTau_M125"        , 3.782*0.0627 },
     { "ZHToTauTau_M125"          , 0.0554 },
     { "WplusHToTauTau_M125"      , 0.0527 },
-    { "WminusHToTauTau_M125"     , 0.0358 },
+    { "WminusHToTauTau_M125"     , 0.0334 },
     { "GluGluHToWWTo2L2Nu_M125"  , 1.091343 }, // see 2018
     { "VBFHToWWTo2L2Nu_M125"     , 0.084962 }, // see 2018
     { "ttHJetToTT_M125"          , 0.5071*0.0627 } // see 2017
@@ -300,6 +305,37 @@ map<TString, int> n_events_per_sample_2017 = {
 {"ttHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8" , 21713806},
 };
 //TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8, TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8, TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8 , ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8, ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8, ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8, ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8, GluGluHToTauTau_M125_13TeV_powheg_pythia8 can be normalized using GetSumOfWeights()
+
+map<TString, int> n_events_per_sample_2016 = {
+{"WJetsToLNu"  , 86916455},
+{"W1JetsToLNu" , 45283121},
+{"W2JetsToLNu" , 30374504},
+{"W3JetsToLNu" , 39501912},
+{"W4JetsToLNu" , 20824737},
+{"ZZ" , 1988098},
+{"WW" , 7982180},
+{"WZ" , 3997571},
+{"DYJetsToLL_M-10to50" , 35114961},
+{"DYJetsToLL_M-50"     , 146280395},
+{"DY1JetsToLL_M-50"    , 63730337},
+{"DY2JetsToLL_M-50"    , 19879279},
+{"DY3JetsToLL_M-50"    , 5857441},
+{"DY4JetsToLL_M-50"    , 4197868},
+{"TTbar" , 76915549},
+{"ST_t-channel_top"     , 67105876},
+{"ST_t-channel_antitop" , 38811017},
+{"ST_tW_top"            , 6952830},
+{"ST_tW_antitop"        , 6933094},
+{"EWKWPlus2Jets"            ,  4943595},
+{"EWKWMinus2Jet"            ,  4782075},
+{"EWKZ2Jets_ZToLL"          ,  999451 },
+{"EWKZ2Jets_ZToNuNu"        , 2985280 },
+{"WGToLNuG"                 ,  27511845},
+{"WGstarToLNuMuMu"          ,  2218596},
+{"WGstarToLNuEE"            ,  2149729},
+};
+//signal samples can be normalized using GetSumOfWeights()
+
 
 double getNEventsProcessed(TString filename)
 {
