fb5110d2c7830707c8e03c6998cbc30e4c345fe7
mmeyer <mareike.meyer@desy.de>
2019-12-19 14:02:15 +0100

    updated to new CombineHarvester version

--- DNN/GoF/run_gof.sh
+++ DNN/GoF/run_gof.sh
@@ -14,22 +14,23 @@ NUM_TOYS=300
 CURRENT_PATH=$(pwd)
 BASE_PATH=${CURRENT_PATH}/output/${ERA}
 OUTPUT_FOLDER=${ERA}_${VAR}_smhtt
-#CMSSW_LOCATION=/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/CombineHarvester/2017/CMSSW_8_1_0/src
-CMSSW_LOCATION=/nfs/dust/cms/user/tlenz/13TeV/2018/CMSSW/SMHTauTau/CMSSW_8_1_0/src
+CMSSW_LOCATION=/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/CombineHarvester/Run2Analysis/CMSSW_10_2_16/src/
+#CMSSW_LOCATION=/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/CombineHarvester/LegacyAnalysis/CMSSW_8_1_0/src
+#CMSSW_LOCATION=/nfs/dust/cms/user/tlenz/13TeV/2018/CMSSW/SMHTauTau/CMSSW_8_1_0/src
 
-export SCRAM_ARCH=slc6_amd64_gcc530
+export SCRAM_ARCH=slc6_amd64_gcc700
 cd ${CMSSW_LOCATION}
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cmsenv
 
-cd ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017
+cd ${CMSSW_BASE}/src/CombineHarvester/SMRun2Legacy
 
 echo "ERA is " $ERA
 
 echo "Input folder is " $INPUT_FOLDER
 echo "var is " $VAR
 
-MorphingSM2017 \
+MorphingSMRun2Legacy \
     --base_path=${BASE_PATH} \
     --input_folder_em=$INPUT_FOLDER \
     --real_data=true \
@@ -49,9 +50,9 @@ MorphingSM2017 \
     --rebin_categories=false
 
 # Create workspace
-combineTool.py -M T2W -o ${ERA}_workspace.root -m $MASS -i ${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/${OUTPUT_FOLDER}/cmb/125/
+combineTool.py -M T2W -o ${ERA}_workspace.root -m $MASS -i ${CMSSW_BASE}/src/CombineHarvester/Run2Analysis/output/${OUTPUT_FOLDER}/cmb/125/
 
-workspace_location=${CMSSW_BASE}/src/CombineHarvester/HTTSM2017/output/${OUTPUT_FOLDER}/cmb/125/
+workspace_location=${CMSSW_BASE}/src/CombineHarvester/Run2Analysis/output/${OUTPUT_FOLDER}/cmb/125/
 cd ${workspace_location}
 
 # Get test statistic value
